4bd017e11209fa6df392a2f97f7fdecb024c0832
Louis Bouchard <louis.bouchard@cctt-optech.ca>
2021-02-02 17:16:29 -0500

    measurements!

--- tools/util.py
+++ tools/util.py
@@ -1,6 +1,10 @@
+import math
+
 import cv2 as cv
 import numpy as np
 import os
+import random
+import matplotlib.pyplot as plt
 
 
 def clean_folders(output_paths, ext=".png"):
@@ -71,3 +75,391 @@ def formatage(n, world, computerImage, Cx, Cy, dx, dy, R, T, sx, f_, Tz_, k1_, k
     # Distortion coefficients
     distCoeffs = np.array([k1_, k2_,0,0])
     return imagePoints, objectPoints, rvec, tvec, cameraMatrix, distCoeffs
+
+
+def unhom(a):
+    if not isinstance(a, np.ndarray):
+        aa = np.array(a)
+    else:
+        aa = a
+    if aa[-1] == 0:
+        return aa[:-1]
+    return aa[:-1]/aa[-1]
+
+def decimateRandom(pts1, pts2, proportion):
+    n = min(pts1.shape[0], pts2.shapr[0])
+    k = int(n * proportion)
+    newPts1 = []
+    newPts2 = []
+    for i in range(0, k):
+        r = random.randrange(0, n)
+        newPts1.append(pts1[r])
+        newPts2.append(pts2[r])
+
+    if isinstance(pts1, np.ndarray):
+        newPts1 = np.array(newPts1)
+    if isinstance(pts2, np.ndarray):
+        newPts2 = np.array(newPts2)
+
+    return newPts1, newPts2
+
+def decimate(pts1, pts2, proportion):
+    n = min(pts1.shape[0], pts2.shapr[0])
+    k = int(n * proportion)
+    newPts1 = []
+    newPts2 = []
+    for i in range(0, k):
+        r = random.randrange(0, n)
+        newPts1.append(pts1[r])
+        newPts2.append(pts2[r])
+
+    if isinstance(pts1, np.ndarray):
+        newPts1 = np.array(newPts1)
+    if isinstance(pts2, np.ndarray):
+        newPts2 = np.array(newPts2)
+
+    return newPts1, newPts2
+
+def transformSinglePoint3d(pt, mat):
+    # pt: (3,1);  mat:(3,4)
+    pt = np.append(pt, [1])
+    mat = np.r_[mat, [[0,0,0,1]]]
+    #print("pt", pt)
+    #print("mat\n", mat)
+    a = mat.dot(pt)
+    #print("a", a)
+    return unhom(a)
+
+def transformPoints3d(pt, mat):
+    # pt: (3,1);  mat:(3,4)
+    if len(pt.shape) == 1:
+        return None
+    #pt = np.append(pt, [1])
+    nbpts = pt.shape[0]
+    #print("pt1", pt)
+    pt = np.c_[pt, np.ones(nbpts)]
+    mat = np.r_[mat, [[0,0,0,1]]]
+    #print("pt2", pt)
+    #print("mat\n", mat)
+    a = np.transpose(mat.dot(np.transpose(pt)))
+    #print("a\n", a)
+    b = cv.convertPointsFromHomogeneous(a)
+    b = b.reshape((b.shape[0], b.shape[-1]))
+    #print("b\n", b)
+    return b
+
+def makeR4x4(R, T=None):
+    if T is None:
+        T = [0.0, 0.0, 0.0]
+    M = np.c_[R, T]
+    M = np.r_[M, [[0.0, 0.0, 0.0, 1.0]]]
+    return M
+
+def triangulate(campts, projpts, cameraIntrMatrix3x3, projIntrMatrix3x3, camDistCoeffs, projDistCoeffs, R, T):
+
+    undistCamPts = cv.undistortPoints(campts, cameraIntrMatrix3x3, camDistCoeffs)
+    undistProjPts = cv.undistortPoints(projpts, projIntrMatrix3x3, projDistCoeffs)
+
+    undistCamPts = undistCamPts.reshape((undistCamPts.shape[0], undistCamPts.shape[-1]))
+    undistProjPts = undistProjPts.reshape((undistProjPts.shape[0], undistProjPts.shape[-1]))
+
+    M1 = np.eye(3, 4, dtype=float)
+    M2 = np.c_[R, T]
+    """
+    print("*******************************************")
+    print("M1", M1.shape, "\n", M1)
+    print("*******************************************")
+    print("M2", M2.shape, "\n", M2)
+    print("*******************************************")
+    """
+    reconpts4 = cv.triangulatePoints(M1, M2, np.transpose(undistCamPts), np.transpose(undistProjPts))
+    reconpts4 = np.transpose(reconpts4)
+    n = reconpts4.shape[0]
+
+    reconpts = np.zeros((n, 3), dtype=float)
+    for i in range(0, n):
+        rp = unhom(reconpts4[i])
+        reconpts[i] = rp
+
+    return reconpts
+
+
+def measureAligned(objpts, reconpts):
+    n = objpts.shape[0]
+    dists = []
+    for i in range(0, n):
+        op = objpts[i]
+        rp = reconpts[i]
+        d = np.linalg.norm(op-rp)
+        dists.append(d)
+        #print(op, "  -  ", rp, "  =  ", d)
+
+    #print("dists:\n", np.array(dists))
+
+    minDist = min(dists)
+    maxDist = max(dists)
+    avgDist = np.mean(dists)
+    stdDist = np.std(dists)
+    """
+    print("minDist", minDist)
+    print("maxDist", maxDist)
+    print("avgDist", avgDist)
+    print("stdDist", stdDist)"""
+    return minDist, maxDist, avgDist, stdDist
+
+
+def measureSimDist(objpts, reconpts):
+    """
+    print("*******************************************")
+    print("objpts", objpts.shape, "\n", objpts)
+    print("*******************************************")
+    print("reconpts", reconpts.shape, "\n", reconpts)
+    print("*******************************************")
+    """
+    dists = []
+
+    n = objpts.shape[0]
+    """ only if aligned!!!!!!!!!
+    for i in range(0, n):
+        op = objpts[i]
+        rp = reconpts[i]
+        d = np.linalg.norm(op-rp)
+        dists.append(d)
+        #print(op, "  -  ", rp, "  =  ", d)
+
+    minDist = min(dists)
+    maxDist = max(dists)
+    avgDist = np.mean(dists)
+    stdDist = np.std(dists)
+
+    print("minDist", minDist)
+    print("maxDist", maxDist)
+    print("avgDist", avgDist)
+    print("stdDist", stdDist)
+    """
+    op0 = objpts[0]
+    rp0 = reconpts[0]
+    #print(op0)
+    #print(rp0)
+    distDiffs = []
+    for i in range(1, n):
+        #print("-----------------------------")
+        op = objpts[i]
+        rp = reconpts[i]
+        #print(op)
+        #print(rp)
+        d1 = np.linalg.norm(op0 - op)
+        d2 = np.linalg.norm(rp0 - rp)
+        dd = abs(d1-d2)
+        distDiffs.append(dd)
+        #print(d1, " .. ", d2, " == ", dd)
+
+    minDD = min(distDiffs)
+    maxDD = max(distDiffs)
+    avgDD = np.mean(distDiffs)
+    stdDD = np.std(distDiffs)
+    """
+    print("minDD", minDD)
+    print("maxDD", maxDD)
+    print("avgDD", avgDD)
+    print("stdDD", stdDD)"""
+    return minDD, maxDD, avgDD, stdDD
+
+
+def listPlot(xData, yData, title, xLabel, yLabel, savePath=None, ySubLabels=None):
+    ##x = np.linspace(0, 2, 100)
+    # Note that even in the OO-style, we use `.pyplot.figure` to create the figure.
+    fig, ax = plt.subplots()  # Create a figure and an axes.
+
+    if not isinstance(yData, np.ndarray):
+        yData = np.array(yData)
+
+    nbx = len(xData)
+    print("nbx",nbx)
+    print("yshape", yData.shape)
+
+    if len(yData.shape) < 2:
+        ax.plot(xData, yData)
+    else:
+
+        if yData.shape[0] == nbx:
+            yData = yData.transpose()
+        elif yData.shape[1] == nbx:
+            pass
+        else:
+            #error
+            exit()
+
+        nbData = yData.shape[0]
+        print("nbData", nbData)
+        for i in range(0, nbData):
+            d = yData[i, :]
+            lbl = "data" + str(i)
+            if ySubLabels is not None:
+                lbl = ySubLabels[i]
+            print("d.shape", d.shape)
+            ax.plot(xData, d, label=lbl)
+
+    ##ax.plot(x, x, label='linear')  # Plot some data on the axes.
+    ##ax.plot(x, x ** 2, label='quadratic')  # Plot more data on the axes...
+    ##ax.plot(x, x ** 3, label='cubic')  # ... and some more.
+    ax.set_xlabel(xLabel)  # Add an x-label to the axes.
+    ax.set_ylabel(yLabel)  # Add a y-label to the axes.
+    ax.set_title(title)  # Add a title to the axes.
+    ax.legend()  # Add a legend.
+    if savePath is None:
+        plt.show()
+    else:
+        fig.savefig(savePath)
+
+
+def calibrateExtrinsic(objp, imgp, projp, cameraMatrix, camDistCoeffs, projMatrix, projDistCoeffs):
+
+    method = cv.SOLVEPNP_ITERATIVE
+    if objp.shape[0] == 4:
+        method = cv.SOLVEPNP_AP3P
+
+    retval, rvec, camT = cv.solvePnP(objp, imgp, cameraMatrix, camDistCoeffs, flags=method)
+    if math.isnan(np.sum(camT)) or not retval:
+        #print("solvePnP cam: bad set of points!")
+        raise Exception("bad set of points.  solvePnP retval=", retval, "camT=", camT)
+
+    #print("retval", retval)
+    #print("rvec", rvec)
+    camR, _ = cv.Rodrigues(rvec)
+    #print("camR:\n", camR)
+    #print("camT", camT)
+
+    retval, rvec, projT = cv.solvePnP(objp, projp, projMatrix, projDistCoeffs, flags=method)
+    if math.isnan(np.sum(projT)) or not retval:
+        #print("solvePnP proj: bad set of points!")
+        raise Exception("bad set of points.  solvePnP retval=", retval, "projT=", projT)
+
+    #print("retval", retval)
+    #print("rvec", rvec)
+    projR, _ = cv.Rodrigues(rvec)
+    #print("projR:\n", projR)
+    #print("projT", projT)
+
+    cam4x4 = makeR4x4(camR, camT)
+    proj4x4 = makeR4x4(projR, projT)
+    iCam4x4 = np.linalg.inv(cam4x4)
+    M = proj4x4.dot(iCam4x4)
+    print("M=\n", M)
+
+    R = M[0:3, 0:3]
+    T = M[0:3, 3]
+    #print("R=\n", R)
+    #print("T=\n", T)
+    return R, T
+    """
+     [[ 0.99750773 -0.01969474  0.06775279]
+ [ 0.01730883  0.99921537  0.03562359]
+ [-0.06840123 -0.03436208  0.99706596]]
+T
+ [[-0.61477313]
+ [-0.00765998]
+ [-0.28205569]]
+    """
+
+
+def measureold(objpts, imgpts1, imgpts2, K1, K2, coeffs1, coeffs2, R, T):
+    print("*******************************************")
+    print("objpts", objpts.shape, "\n", objpts)
+    print("*******************************************")
+    print("imgpts1", imgpts1.shape, "\n", imgpts1)
+    print("*******************************************")
+    print("imgpts2", imgpts2.shape, "\n", imgpts2)
+    print("*******************************************")
+    """
+    M1 = np.c_[K1, [0.0, 0.0, 1.0]]
+    print("*******************************************")
+    print("**M1", M1.shape, "\n", M1)
+    M1 = np.r_[M1, [[0.0, 0.0, 0.0, 1.0]]]
+    print("*******************************************")
+    print("**M1", M1.shape, "\n", M1)
+
+    M2 = np.c_[K2, [0.0, 0.0, 0.0]]
+    M2 = np.r_[M2, [[0.0, 0.0, 0.0, 1.0]]]
+
+    RR = np.c_[R,  [0.0, 0.0, 0.0]]
+    RR = np.r_[RR, [[0.0, 0.0, 0.0, 1.0]]]
+
+    TT = np.eye(3, dtype=float)
+    TT = np.c_[TT, T]
+    TT = np.r_[TT, [[0.0, 0.0, 0.0, 1.0]]]
+
+    M2 = M2.dot(RR).dot(TT)
+
+    M1 = M1[:-1]
+    M2 = M2[:-1]
+    """
+
+    n = objpts.shape[0]
+
+    """
+    M1 = np.eye(3, 4, dtype=float)
+    M2 = np.c_[ R, T ]
+
+    print("*******************************************")
+    print("M1", M1.shape, "\n", M1)
+    print("*******************************************")
+    print("M2", M2.shape, "\n", M2)
+    print("*******************************************")
+    reconpts4 = cv.triangulatePoints(M1, M2, np.transpose(imgpts1), np.transpose(imgpts2))
+    reconpts4 = np.transpose(reconpts4)
+    reconpts = np.zeros((n,3), dtype=float)
+    for i in range(0,n):
+        rp = unhom(reconpts4[i])
+        reconpts[i] = rp
+
+    """
+    reconpts = triangulate(imgpts1, imgpts2, K1, K2)
+    print("reconpts", reconpts.shape, "\n", reconpts)
+    print("*******************************************")
+
+    dists = []
+
+    for i in range(0, n):
+        op = objpts[i]
+        rp = reconpts[i]
+        d = np.linalg.norm(op-rp)
+        dists.append(d)
+        #print(op, "  -  ", rp, "  =  ", d)
+
+    minDist = min(dists)
+    maxDist = max(dists)
+    avgDist = np.mean(dists)
+    stdDist = np.std(dists)
+
+    print("minDist", minDist)
+    print("maxDist", maxDist)
+    print("avgDist", avgDist)
+    print("stdDist", stdDist)
+
+    op0 = objpts[0]
+    rp0 = reconpts[0]
+    #print(op0)
+    #print(rp0)
+    distDiffs = []
+    for i in range(1, n):
+        print("-----------------------------")
+        op = objpts[i]
+        rp = reconpts[i]
+        #print(op)
+        #print(rp)
+        d1 = np.linalg.norm(op0 - op)
+        d2 = np.linalg.norm(rp0 - rp)
+        dd = abs(d1-d2)
+        distDiffs.append(dd)
+        print(d1, " .. ", d2, " == ", dd)
+
+    minDD = min(distDiffs)
+    maxDD = max(distDiffs)
+    avgDD = np.mean(distDiffs)
+    stdDD = np.std(distDiffs)
+
+    print("minDD", minDD)
+    print("maxDD", maxDD)
+    print("avgDD", avgDD)
+    print("stdDD", stdDD)
--- zhang_far_range.py
+++ zhang_far_range.py
@@ -1,19 +1,35 @@
 """ calibration Zhang pour intrinsèques et avec les cibles pour extrinsèques """
+import math
 
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 import os
 import glob
-
+import random
 from tools.findPoints import proj_centers
-from tools.util import coins_damier, clean_folders, draw_reprojection
+from tools.util import coins_damier, clean_folders, draw_reprojection, measureSimDist, measureAligned, triangulate, transformPoints3d, calibrateExtrinsic, listPlot
+"""
+print(len(np.array([1,2,4]).shape))
 
+m = [[2,0,0,1],[0,2,0,1],[0,0,2,1]]
+m = np.array(m)
+p = np.array([1,2,3])
+a = transformSinglePoint3d(p, m)
+print(a)
+
+pp = np.array([[1,2,3],[3,2,1]])
+b = transformPoints3d(pp, m)
+print(b)
+exit()
+"""
 # Data ========================================================================
 dataPath="data/14_01_2021/"
 # Data pour intrinsèque:
-noFringePath=np.sort(glob.glob(os.path.join(dataPath,"zhang/max*.png")))
-sgmfPath=np.sort(glob.glob(os.path.join(dataPath,"zhang/match*.png")))
+#noFringePath=np.sort(glob.glob(os.path.join(dataPath,"zhang/max*.png")))
+#sgmfPath=np.sort(glob.glob(os.path.join(dataPath,"zhang/match*.png")))
+noFringePath=np.sort(glob.glob("/home/lbouchard/data/crvi/14jan/zhang/*/max_00.png"))
+sgmfPath=np.sort(glob.glob("/home/lbouchard/data/crvi/14jan/zhang/*/match_00.png"))
 # Data pour extrinsèque:
 objpRT_path=os.path.join(dataPath,"RT-calib/objpts.txt")
 imgpRT_path=os.path.join(dataPath,"RT-calib/imgpts.txt")
@@ -87,8 +103,8 @@ projPoints_=projPoints.copy()
 objectPoints_.append(objpRT)
 imagePoints_.append(imgpRT)
 projPoints_.append(projpRT)
-#===============================================================================
 
+#===============================================================================
 #==== CALIBRATION INTRINSÈQUE ==================================================
 # Camera -----------------------------------------------------------------------
 retC, cameraMatrix, camDistCoeffs, rvecs, tvecs, stdDeviationsIntrinsics, stdDeviationsExtrinsics, perViewErrors=cv.calibrateCameraExtended(objectPoints_, imagePoints_, imageSize, np.zeros((3,3)), np.zeros((1,4)))
@@ -101,9 +117,272 @@ retP, projMatrix, projDistCoeffs, rvecs, tvecs, stdDeviationsIntrinsics, stdDevi
 
 #==== CALIBRATION EXTRINSÈQUE ==================================================
 retval, cameraMatrix, camDistCoeffs, projMatrix, projDistCoeffs, R, T, _, _, perViewErrors = cv.stereoCalibrateExtended([objpRT], [imgpRT], [projpRT], cameraMatrix, camDistCoeffs, projMatrix, projDistCoeffs, imageSize, None, None, flags=cv.CALIB_FIX_INTRINSIC)
+#print("R\n", R)
+#print("T\n", T)
+#calibrateExtrinsic(objpRT, imgpRT, projpRT, cameraMatrix, camDistCoeffs, projMatrix, projDistCoeffs)
+originalT = T.flatten()
+originalR = R.copy()
 #===============================================================================
 color=cv.imread("data/14_01_2021/RT-calib/cornerSubPix.png")
 img =draw_reprojection(color, outputPath, objpRT, imgpRT, cameraMatrix, camDistCoeffs, (4,6), 1)
+
+print("##################################")
+print("########  MEASUREMENTS  ##########")
+print("##################################")
+
+reconpts = triangulate(imgpRT, projpRT, cameraMatrix, projMatrix, camDistCoeffs, projDistCoeffs, R, T)
+print("reconpts\n", reconpts)
+measureSimDist(objpRT, reconpts)
+#solve...
+#retval, rvec, tvec	=	cv.solvePnP(	objectPoints, imagePoints, cameraMatrix, distCoeffs[, rvec[, tvec[, useExtrinsicGuess[, flags]]]]	)
+#retval, rvecs, tvecs, reprojectionError	=	cv.solvePnPGeneric(	objectPoints, imagePoints, cameraMatrix, distCoeffs[, rvecs[, tvecs[, useExtrinsicGuess[, flags[, rvec[, tvec[, reprojectionError]]]]]]]
+#retval, rvec, tvec, inliers	=	cv.solvePnPRansac(	objectPoints, imagePoints, cameraMatrix, distCoeffs[, rvec[, tvec[, useExtrinsicGuess[, iterationsCount[, reprojectionError[, confidence[, inliers[, flags]]]]]]]]	)
+#refine...
+#rvec, tvec = cv.solvePnPRefineLM(objectPoints, imagePoints, cameraMatrix, distCoeffs, rvec, tvec[, criteria]    )
+#rvec, tvec	=	cv.solvePnPRefineVVS(	objectPoints, imagePoints, cameraMatrix, distCoeffs, rvec, tvec[, criteria[, VVSlambda]]	)
+
+retval, rvec, tvec = cv.solvePnP(objpRT, imgpRT, cameraMatrix, camDistCoeffs)
+
+print("retval", retval)
+print("rvec", rvec)
+R, _ = cv.Rodrigues(rvec)
+print("R:\n", R)
+print("tvec", tvec)
+RT = np.c_[R, tvec]
+print("RT:\n", RT)
+
+transpoints = transformPoints3d(objpRT, RT)
+print("transpoints\n", transpoints)
+measureAligned(reconpts, transpoints)
+#exit()
+
+print("##################################")
+print("##########  DECIMATION  ##########")
+print("##################################")
+###nb = 100
+repeats = 1000
+
+simMinDistData = []
+simMaxDistData = []
+simAvgDistData = []
+simStdDistData = []
+
+alignedMinDistData = []
+alignedMaxDistData = []
+alignedAvgDistData = []
+alignedStdDistData = []
+
+avgSimMinDistdata = []
+avgSimMaxDistdata = []
+avgSimAvgDistdata = []
+avgSimStdDistdata = []
+
+avgAlignedMinDistData = []
+avgAlignedMaxDistData = []
+avgAlignedAvgDistData = []
+avgAlignedStdDistData = []
+
+nbPicks = []
+
+for i in range(4, objpRT.shape[0]):
+    #n = i
+#for i in range(0, nb):
+
+    simMinDistData.append([])
+    simMaxDistData.append([])
+    simAvgDistData.append([])
+    simStdDistData.append([])
+
+    alignedMinDistData.append([])
+    alignedMaxDistData.append([])
+    alignedAvgDistData.append([])
+    alignedStdDistData.append([])
+
+    #u = 0.3
+    #v = u + (1.0 - u) * i / nb
+    #n = int(objpRT.shape[0] * v)
+
+    for j in range(repeats):
+        print(j)
+        #picks = [random.randint(0, objpRT.shape[0]-1) for i in range(n)] # randint inclusive!!.. but bad.. duplicates!
+        success = False
+        while not success:
+            picks = np.random.choice(range(0, objpRT.shape[0]-1), i, replace=False) #replace false -> no dupl
+            print("picks", picks)
+            objp = objpRT[picks]
+            imgp = imgpRT[picks]
+            projp = projpRT[picks]
+            try:
+                if len(picks) == 4:
+                    R, T = calibrateExtrinsic(objp, imgp, projp, cameraMatrix, camDistCoeffs, projMatrix, projDistCoeffs)
+                    print("R=\n", R)
+                    print("T=\n", T)
+                else:
+                    retval, cameraMatrix, camDistCoeffs, projMatrix, projDistCoeffs, R, T, _, _, perViewErrors = cv.stereoCalibrateExtended([objp], [imgp], [projp], cameraMatrix, camDistCoeffs, projMatrix, projDistCoeffs, imageSize, None, None, flags=cv.CALIB_FIX_INTRINSIC)
+                    print("retval", retval)
+                    print("perViewErrors", perViewErrors)
+            except Exception as e:
+                print('Caught this error: ' + repr(e))
+                print("retry with new pts")
+                # exit()
+                continue
+
+            tdist = np.linalg.norm(T.flatten() - originalT)
+            rdist = np.linalg.norm(R - originalR)
+            if tdist > 0.05:
+                print("T", T)
+                print("originalT", originalT)
+                print("diff", T.flatten() - originalT)
+                print("normdiff", tdist)
+                print("bad T: retry with new pts")
+                # exit()
+                continue
+            if rdist > 0.002:
+                print("R", R)
+                print("originalR", originalR)
+                print("diff", R - originalR)
+                print("normdiff", rdist)
+                print("bad R: retry with new pts")
+                # exit()
+                continue
+            success = True
+
+        reconpts = triangulate(imgpRT, projpRT, cameraMatrix, projMatrix, camDistCoeffs, projDistCoeffs, R, T)
+
+        mind, maxd, avgd, stdd = measureSimDist(objpRT, reconpts)
+        if math.isnan(mind) or math.isnan(maxd) or math.isnan(avgd) or math.isnan(stdd):
+            print("objp", objp)
+            print("imgp", imgp)
+            print("projp", projp)
+            print(reconpts)
+            print(mind, maxd, avgd, stdd)
+            print(R)
+            print(T)
+            exit()
+        #simDistdata[-1].append(stdd)
+        simMinDistData[-1].append(mind)
+        simMaxDistData[-1].append(maxd)
+        simAvgDistData[-1].append(avgd)
+        simStdDistData[-1].append(stdd)
+        """
+        if maxd > 1.0:
+            print("bad maxd = ", maxd)
+            print("R\n", R)
+            print("originalR\n", originalR)
+            print("diff", np.linalg.norm(R-originalR))
+            print("T\n", T)
+            print("originalT\n", originalT)
+            print("diff", np.linalg.norm(T - originalT))
+            exit()
+        """
+        mind, maxd, avgd, stdd = measureAligned(reconpts, transpoints)
+        #alignedDistData[-1].append(stdd)
+        alignedMinDistData[-1].append(mind)
+        alignedMaxDistData[-1].append(maxd)
+        alignedAvgDistData[-1].append(avgd)
+        alignedStdDistData[-1].append(stdd)
+        print("-----------------------------------------")
+
+    nbPicks.append(i)
+
+    a = np.mean(np.array(simMinDistData[-1]))
+    avgSimMinDistdata.append(a)
+    a = np.mean(np.array(simMaxDistData[-1]))
+    avgSimMaxDistdata.append(a)
+    a = np.mean(np.array(simAvgDistData[-1]))
+    avgSimAvgDistdata.append(a)
+    a = np.mean(np.array(simStdDistData[-1]))
+    avgSimStdDistdata.append(a)
+
+    a = np.mean(np.array(alignedMinDistData[-1]))
+    avgAlignedMinDistData.append(a)
+    a = np.mean(np.array(alignedMaxDistData[-1]))
+    avgAlignedMaxDistData.append(a)
+    a = np.mean(np.array(alignedAvgDistData[-1]))
+    avgAlignedAvgDistData.append(a)
+    a = np.mean(np.array(alignedStdDistData[-1]))
+    avgAlignedStdDistData.append(a)
+
+print("##################################")
+print("##################################")
+np.set_printoptions(suppress=True)
+
+print("nbpicks", nbPicks)
+#print("simDistdata", np.c_[nbPicks, avgSimDistdata, stdSimDistdata])
+#print("alignedDistData", np.c_[nbPicks, avgAlignedDistData, stdAlignedDistData])
+
+ylbls = ["min", "max", "avg", "std"]
+ydata = np.c_[avgSimMinDistdata, avgSimMaxDistdata, avgSimAvgDistdata, avgSimStdDistdata]
+listPlot(nbPicks, ydata, "Similar distances", "Number of targets", "Distance (m)", "./sim_plt1.png", ySubLabels=ylbls)
+ydata = np.c_[avgAlignedMinDistData, avgAlignedMaxDistData, avgAlignedAvgDistData, avgAlignedStdDistData]
+listPlot(nbPicks, ydata, "Aligned target distances", "Number of targets", "Distance (m)", "./aligned_plt1.png", ySubLabels=ylbls)
+
+print("avgSimMinDistdata", avgSimMinDistdata)
+print("avgSimMaxDistdata", avgSimMaxDistdata)
+print("avgSimAvgDistdata", avgSimAvgDistdata)
+print("avgSimStdDistdata", avgSimStdDistdata)
+
+print("avgAlignedMinDistData", avgAlignedMinDistData)
+print("avgAlignedMaxDistData", avgAlignedMaxDistData)
+print("avgAlignedAvgDistData", avgAlignedAvgDistData)
+print("avgAlignedStdDistData", avgAlignedStdDistData)
+
+print("###################### 4 corners ########################")
+print(objpRT)
+picks = [0, 3, 19, 23]
+print(np.array(objpRT)[picks])
+objp = objpRT[picks]
+imgp = imgpRT[picks]
+projp = projpRT[picks]
+
+R, T = calibrateExtrinsic(objp, imgp, projp, cameraMatrix, camDistCoeffs, projMatrix, projDistCoeffs)
+print("R=\n", R)
+print("T=\n", T)
+reconpts = triangulate(imgpRT, projpRT, cameraMatrix, projMatrix, camDistCoeffs, projDistCoeffs, R, T)
+mind, maxd, avgd, stdd = measureSimDist(objpRT, reconpts)
+print("measureSimDist:")
+print("mind", mind)
+print("maxd", maxd)
+print("avgd", avgd)
+print("stdd", stdd)
+mind, maxd, avgd, stdd = measureAligned(reconpts, transpoints)
+print("measureAligned:")
+print("mind", mind)
+print("maxd", maxd)
+print("avgd", avgd)
+print("stdd", stdd)
+
+
+"""
+x=[0,1,2,3,4,5,6,7]
+y=[[9,8,7,6,5,4,3,2], [3,4,3,4,3,4,3,4]]
+ylbls = ["aaaa", "bbbb"]
+listPlot(x, y, "title", "x", "y", "./plt.png", ySubLabels=ylbls)
+"""
+
+#print("avgAlignedDistData", avgAlignedDistData)
+#print("stdSimDistdata", stdSimDistdata)
+#print("stdAlignedDistData", stdAlignedDistData)
+
+###################################################################################
+# measurements!
+#print("*******************************************")
+#print("cameraMatrix", cameraMatrix.shape, "\n", cameraMatrix)
+#print("*******************************************")
+#print("projMatrix", projMatrix.shape, "\n", projMatrix)
+#print("*******************************************")
+"""
+undistCamPts = cv.undistortPoints(imgpRT, cameraMatrix, camDistCoeffs)
+undistProjPts = cv.undistortPoints(projpRT, projMatrix, projDistCoeffs)
+
+undistCamPts = undistCamPts.reshape((undistCamPts.shape[0], undistCamPts.shape[-1]))
+undistProjPts = undistProjPts.reshape((undistProjPts.shape[0], undistProjPts.shape[-1]))
+
+measure(objpRT, undistCamPts, undistProjPts, cameraMatrix, projMatrix, R, T)
+
+"""
+
+
 
 #==== ENREGISTRER ==============================================================
 # Enregistrer:
@@ -136,3 +415,6 @@ f.write("{}\n".format(retP))
 f.write('Erreur de reprojection RMS stéréo:\n')
 f.write("{}\n".format(retval))
 f.close()
+
+print("done")
+
